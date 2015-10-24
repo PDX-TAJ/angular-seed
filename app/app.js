69af8892cb480c5888670dde0dc8335a3690c37c
t_tappa <alex.tappin@autodesk.com>
2015-10-23 21:15:56 -0700

    Created Service for the API

--- app/app.js
+++ app/app.js
@@ -10,3 +10,17 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+angular.module('myApp.services', []).
+    factory('businessApiService', function($http) {
+      var businessApiService = {};
+      //do they get here
+      //businessApiService.whateverDataIsReturnedGoesHere
+      /*businessApiService.getData = function(){
+
+      };
+      businessApiService.newVariable = whateverYouwWant;*/
+
+      //return the entire object. Access the things in the controllers u use them in.
+      return businessApiService;
+});
